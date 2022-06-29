7afee0c7b16ef8260d2c6d6498f1d0ceadeb0dbf
poseidon <61605433+poseidonKey@users.noreply.github.com>
2022-06-29 21:28:35 +0900

    notes model test

---
+++ lib/memo/notes.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class Notes extends StatelessWidget {
+  const Notes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(label: Text("aa"));
+  }
+}
